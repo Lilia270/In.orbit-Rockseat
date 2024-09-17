23e8152f5a382e316ce1993a4543917304b639fc
Lilia270 <liliarosaestudos@gmail.com>
2024-09-17 11:28:58 -0300

    Front-end do in.orbit

---
+++ src/components/create-goal.tsx
@@ -0,0 +1,172 @@
+import { X } from "lucide-react";
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from "./ui/dialog";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import {
+  RadioGroup,
+  RadioGroupIndicator,
+  RadioGroupItem,
+} from "./ui/radio-group";
+import { Button } from "./ui/button";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createGoal } from "../http/create-goal";
+import { useQueryClient } from "@tanstack/react-query";
+
+const createGoalForm = z.object({
+  title: z.string().min(1, "Informe a atividade que deseja realizar"),
+  desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
+});
+
+type CreateGoalForm = z.infer<typeof createGoalForm>;
+
+export function CreateGoal() {
+  const queryClient = useQueryClient();
+
+  const { register, control, handleSubmit, formState, reset } =
+    useForm<CreateGoalForm>({
+      resolver: zodResolver(createGoalForm),
+    });
+
+  async function handleCreateGoal(data: CreateGoalForm) {
+    await createGoal({
+      title: data.title,
+      desiredWeeklyFrequency: data.desiredWeeklyFrequency,
+    });
+
+    queryClient.invalidateQueries({ queryKey: ["summary"] });
+    queryClient.invalidateQueries({ queryKey: ["pending-goals"] });
+
+    reset();
+  }
+
+  return (
+    <DialogContent>
+      <div className="flex flex-col gap-6 h-full">
+        <div className="flex flex-col gap-3">
+          <div className="flex items-center justify-between">
+            <DialogTitle>Cadastrar meta</DialogTitle>
+            <DialogClose>
+              <X className="size-5 text-zinc-600" />
+            </DialogClose>
+          </div>
+
+          <DialogDescription>
+            Adicione atividades que te fazem bem e que você quer continuar
+            praticando toda semana.
+          </DialogDescription>
+        </div>
+
+        <form
+          onSubmit={handleSubmit(handleCreateGoal)}
+          className="flex-1 flex flex-col justify-between"
+        >
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="title">Qual a atividade?</Label>
+              <Input
+                id="title"
+                autoFocus
+                placeholder="Praticar exercícios, meditar, etc..."
+                {...register("title")}
+              />
+
+              {formState.errors.title && (
+                <p className="text-red-400 text-sm">
+                  {formState.errors.title.message}
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="title">Quantas vezes na semana?</Label>
+              <Controller
+                control={control}
+                name="desiredWeeklyFrequency"
+                defaultValue={3}
+                render={({ field }) => {
+                  return (
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      value={String(field.value)}
+                    >
+                      <RadioGroupItem value="1">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          1x na semana
+                        </span>
+                        <span className="text-lg leading-none">🥱</span>
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="2">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          2x na semana
+                        </span>
+                        <span className="text-lg leading-none">🙂</span>
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="3">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          3x na semana
+                        </span>
+                        <span className="text-lg leading-none">😎</span>
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="4">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          4x na semana
+                        </span>
+                        <span className="text-lg leading-none">😜</span>
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="5">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          5x na semana
+                        </span>
+                        <span className="text-lg leading-none">🤨</span>
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="6">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          6x na semana
+                        </span>
+                        <span className="text-lg leading-none">🤯</span>
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="7">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          Todos dias da semana
+                        </span>
+                        <span className="text-lg leading-none">🔥</span>
+                      </RadioGroupItem>
+                    </RadioGroup>
+                  );
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <DialogClose asChild>
+              <Button type="button" className="flex-1" variant="secondary">
+                Fechar
+              </Button>
+            </DialogClose>
+            <Button className="flex-1">Salvar</Button>
+          </div>
+        </form>
+      </div>
+    </DialogContent>
+  );
+}
---
+++ src/components/summary.tsx
@@ -0,0 +1,114 @@
+import { CheckCircle2, Plus } from "lucide-react";
+import { Button } from "./ui/button";
+import { DialogTrigger } from "./ui/dialog";
+import { InOrbitIcon } from "./in-orbit-icon";
+import { Progress, ProgressIndicator } from "./ui/progress-bar";
+import { Separator } from "./ui/separator";
+import { OutlineButton } from "./ui/outline-button";
+import { useQuery } from "@tanstack/react-query";
+import { getSummary } from "../http/get-summary";
+import dayjs from "dayjs";
+import ptBR from "dayjs/locale/pt-BR";
+import { PendingGoals } from "./pending-goals";
+
+dayjs.locale(ptBR);
+
+export function Summary() {
+  const { data } = useQuery({
+    queryKey: ["summary"],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, // 60 seconds
+  });
+
+  if (!data) {
+    return null;
+  }
+
+  const firstDayOfWeek = dayjs().startOf("week").format("D MMM");
+  const lastDayOfWeek = dayjs().endOf("week").format("D MMM");
+
+  const completedPercentage = Math.round((data.completed * 100) / data.total);
+
+  return (
+    <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <InOrbitIcon />
+          <span className="text-lg font-semibold capitalize">
+            {firstDayOfWeek} - {lastDayOfWeek}
+          </span>
+        </div>
+
+        <DialogTrigger asChild>
+          <Button size="sm">
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <Progress value={8} max={15}>
+          <ProgressIndicator style={{ width: `${completedPercentage}%` }} />
+        </Progress>
+
+        <div className="flex items-center justify-between text-xs text-zinc-400">
+          <span>
+            Você completou{" "}
+            <span className="text-zinc-100">{data?.completed}</span> de{" "}
+            <span className="text-zinc-100">{data?.total}</span> metas nessa
+            semana.
+          </span>
+          <span>{completedPercentage}%</span>
+        </div>
+      </div>
+
+      <Separator />
+
+      <PendingGoals />
+
+      <div className="flex flex-col gap-6">
+        <h2 className="text-xl font-medium">Sua semana</h2>
+
+        {data.goalsPerDay && Object.entries(data.goalsPerDay).length > 0 ? (
+          Object.entries(data.goalsPerDay).map(([date, goals]) => {
+            const weekDay = dayjs(date).format("dddd");
+            const formattedDate = dayjs(date).format("D[ de ]MMMM");
+
+            return (
+              <div key={date} className="flex flex-col gap-4">
+                <h3 className="font-medium">
+                  <span className="capitalize">{weekDay}</span>{" "}
+                  <span className="text-zinc-400 text-xs">
+                    ({formattedDate})
+                  </span>
+                </h3>
+
+                <ul className="flex flex-col gap-3">
+                  {goals.map((goal) => {
+                    const time = dayjs(goal.completedAt).format("HH:mm");
+
+                    return (
+                      <li key={goal.id} className="flex items-center gap-2">
+                        <CheckCircle2 className="size-4 text-pink-500" />
+                        <span className="text-sm text-zinc-400">
+                          Você completou "
+                          <span className="text-zinc-100">{goal.title}</span>"
+                          às <span className="text-zinc-100">{time}h</span>
+                        </span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            );
+          })
+        ) : (
+          <p className="text-center text-zinc-400">
+            Nenhuma meta registrada para esta semana.
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
